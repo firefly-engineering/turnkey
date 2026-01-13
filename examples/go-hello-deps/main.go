fc63badc4d8e9ef4809f5633d2d9cf0663e9b23f
Yann Hodique <yann.hodique@gmail.com>
2026-01-13 22:12:09 +0100

    test: add golang.org/x/sys/cpu to go-hello-deps example

--- examples/go-hello-deps/main.go
+++ examples/go-hello-deps/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/google/uuid"
+	"golang.org/x/sys/cpu"
 )
 
 func main() {
 	id := uuid.New()
 	fmt.Printf("Hello from turnkey! Generated UUID: %s\n", id)
+
+	// Use golang.org/x/sys/cpu to demonstrate assembly-based dependency
+	fmt.Printf("Running on %s/%s\n", runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("CPU has AVX: %v\n", cpu.X86.HasAVX)
+	fmt.Printf("CPU has SSE4.2: %v\n", cpu.X86.HasSSE42)
 }
